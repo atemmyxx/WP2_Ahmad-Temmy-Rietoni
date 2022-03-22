ef277089eeaacb25be7f99570cb5bcd8649c9c83
atemmyxx <@ahmadtemmy20@gmail.com>
2022-03-22 10:26:14 +0700

    ubah file latihan

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -9,7 +9,7 @@ public function penjumlahan($n1, $n2)
 {
     $this -> load-> model('model_latihan1');
     $hasil = $this-> model_latihan1-> jumlah($n1, $n2);
-    echo "hasil Penjumlahan dari" . $n1 ." + ". $n2 ." = "
+    echo "hasil Penjumlahan dari " . $n1 ." + ". $n2 ." = "
     .$hasil;
 }
 
