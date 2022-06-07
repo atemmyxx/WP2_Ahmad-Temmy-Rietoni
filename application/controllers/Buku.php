322e7e775f3f6ffe90c4c9f3821f9591b5759e66
atemmyxx <ahmadtemmy20@gmail.com>
2022-06-07 10:02:37 +0700

    modifikasi file pustaka-bboking

--- application/controllers/Buku.php
+++ application/controllers/Buku.php
@@ -5,7 +5,7 @@ class Buku extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        cek_login();
+        // cek_login();
     }
     //manajemen Buku
     public function index()
@@ -111,5 +111,38 @@ Penerbit', 'required|min_length[3]', [
             $this->ModelBuku->simpanBuku($data);
             redirect('buku');
         }
+    }
+    public function kategori()
+    {
+        $data['judul'] = 'Kategori Buku';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+        $data['kategori'] = $this->ModelBuku->getKategori()->result_array();
+        $this->form_validation->set_rules(
+            'kategori',
+            'Kategori',
+            'required',
+            [
+                'required' => 'Judul Buku harus diisi'
+            ]
+        );
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('buku/kategori', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $data = [
+                'kategori' => $this->input->post('kategori')
+            ];
+            $this->ModelBuku->simpanKategori($data);
+            redirect('buku/kategori');
+        }
+    }
+    public function hapusKategori()
+    {
+        $where = ['id' => $this->uri->segment(3)];
+        $this->ModelBuku->hapusKategori($where);
+        redirect('buku/kategori');
     }
 }
