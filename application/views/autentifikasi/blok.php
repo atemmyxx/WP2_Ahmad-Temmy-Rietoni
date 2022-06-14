06c29e5ca4a4931369877e93af462415887fda44
atemmyxx <ahmadtemmy20@gmail.com>
2022-06-14 10:31:52 +0700

    update pustaka booking pertemuan 13

--- application/views/autentifikasi/blok.php
+++ application/views/autentifikasi/blok.php
@@ -9,10 +9,10 @@
     <meta name="author" content="">
     <title>Akses diblock </title>
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('/'); ?>assets/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <!-- Custom styles for this template-->
-    <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?= base_url('/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
@@ -65,14 +65,14 @@
     </div>
     <!-- Bootstrap core JavaScript-->
     <script src="<?=
-                    base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+                    base_url('/'); ?>assets/jquery/jquery.min.js"></script>
     <script src="<?=
-                    base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js">
+                    base_url('/'); ?>assets/bootstrap/js/bootstrap.bundle.min.js">
     </script>
     <!-- Core plugin JavaScript-->
-    <script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?= base_url('/'); ?>assets/jquery-easing/jquery.easing.min.js"></script>
     <!-- Custom scripts for all pages-->
-    <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
+    <script src="<?= base_url('/'); ?>js/sb-admin-2.min.js"></script>
 </body>
 
 </html>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -37,14 +37,14 @@
     </div>
 </div>
 <!-- Bootstrap core JavaScript-->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/jquery/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- Core plugin JavaScript-->
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="assets/jquery-easing/jquery.easing.min.js"></script>
 <!-- Custom scripts for all pages-->
 <script src="js/sb-admin-2.min.js"></script>
 <!-- Page level plugins -->
-<script src="vendor/chart.js/Chart.min.js"></script>
+<script src="assets/chart.js/Chart.min.js"></script>
 <!-- Page level custom scripts -->
 <script src="js/demo/chart-area-demo.js"></script>
 <script src="js/demo/chart-pie-demo.js"></script>
